ba42caa47d2adfb8e74cafaeb1a0121f1c735fd4
caseysims <caseysims07@gmail.com>
2026-03-11 14:11:58 +0000

    Book class done

---
+++ src/main/java/com/caseysims/Book.java
@@ -0,0 +1,33 @@
+package com.caseysims;
+
+public class Book
+{
+    String title;
+    String author;
+    String genre;
+    int year;
+
+    public Book(String title, String author, int year,String genre)
+    {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
